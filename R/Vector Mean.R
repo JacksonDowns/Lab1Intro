0e6ac1fdd072bae14b83a0ebf4b5dd367cbbb2ae
Jackson Downs <jrdowns@ou.edu>
2020-01-19 19:52:11 -0600

    added some descriptions to the roxygen2 skeleton.

--- R/Vector Mean.R
+++ R/Vector Mean.R
@@ -1,11 +1,17 @@
 #' Title
 #'
-#' @param x
+#' @param x a vector object
 #'
-#' @return
+#' @return The meean of the vector object passed as a parameter
 #' @export
 #'
-#' @examples
+#' @examples The mean of a sample vector [1,1,1,1] = 1. The mean of a vector [1,2,3,4,5] = 3
+#'
+#'
 vectorMean = function(x){
+
+  mean(x)
+
+
 
 }
