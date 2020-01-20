ff425691c7ff6b187f63e857a7f73fef826ba9e6
Jackson Downs <jrdowns@ou.edu>
2020-01-20 17:27:38 -0600

    combined functions onto one R script and created an internal script to be used within the package.

--- R/Internal.R
+++ R/Internal.R
@@ -5,34 +5,34 @@
 #' @param vectory 2nd vector parameter
 #'
 #' @return returns the covariance of the two vectors
-#' @export
 #'
-#' @examples
+#'
+#'
 Covariance = function(vectorx, vectory){
 
+  #top part of the covariance function
   sumOfDifferences = 0
 
+  #Mean value of the first vector parameter
   xbar = VectorMean(vectorx)
+
+  #Mean values of the second vector parameter
   ybar = VectorMean(vectory)
+
+  #length of the two vectors
   n = length(vectorx)
 
+  #Sums the differences of the vector values - their respective means and multiplies the results
   for(i in 1:n) {
 
     sumOfDifferences = sumOfDifferences + ((vectorx[i] - xbar) * (vectory[i] - ybar))
 
   }
 
+  #Calculates the covariance
   covar = sumOfDifferences / (n-1)
 
+  #Returns the covariance of the vectors
   return(covar)
 
 }
-
-
-
-
-
-
-
-
-
---
+++ R/Turn in Functions.R
@@ -0,0 +1,36 @@
+#' Vector Mean
+#'
+#' @param x a vector object
+#'
+#' @return The meean of the vector object passed as a parameter
+#' @export
+#'
+#' @examples The mean of a sample vector [1,1,1,1] = 1. The mean of a vector [1,2,3,4,5] = 3
+#'
+#'
+VectorMean = function(x){
+
+  #returns the mean of the parameter
+  return(mean(x))
+
+}
+
+
+#'Covariance matrix function
+#'
+#' @param matrix matrix of vectors, with the number of vectors represented by rows, and the amount of data represented as columns
+#'
+#' @return a matrix of the covariances of all the vectors
+#' @export
+#'
+#' @examples
+CovarianceMatrix = function(matrix){
+
+
+
+
+
+
+}
+
+
